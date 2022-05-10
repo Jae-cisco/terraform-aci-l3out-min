aa2bd4d2febbf0bfb8356257622ec8f0c0685a6a
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-05-10 16:40:09 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,3 +8,15 @@ terraform {
     }
   }
 }
+
+#configure provider with your cisco aci credentials.
+provider "aci" {
+  # cisco-aci user name
+  username = var.username
+  # cisco-aci password
+  password = var.password
+  # cisco-aci url
+  url      = var.url
+  insecure = true
+  #proxy_url = "https://proxy_server:proxy_port"
+}
