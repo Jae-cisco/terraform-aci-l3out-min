822f2168e9734d303eaadd0bce6615d062fd4c95
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-05-10 16:44:12 +0900

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "username" {
+  type        = string
+  description = "username of ACI"
+}
+variable "password" {
+  type        = string
+  description = "password of ACI"
+}
+variable "url" {
+  type        = string
+  description = "url of ACI"
+}
