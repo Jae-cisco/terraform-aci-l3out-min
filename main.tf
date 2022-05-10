65ea4e1488f86bcd7d2b13dc33e4ddf68cf58e57
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-05-10 20:52:03 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+resource "aci_tenant" "cisco_it_tenant" {
+	name        = "terraform-jaheo-tenant"
+	description = "ACI Tenant for internal IT"
+}
+
+resource "aci_vrf" "cisco_it_vrf" {
+	tenant_dn   = "${aci_tenant.cisco_it_tenant.id}"
+	name        = "terraform-jaheo-vrf"
+}
+
+resource "aci_bridge_domain" "cisco_it_bd" {
+	tenant_dn          = "${aci_tenant.cisco_it_tenant.id}"
+	relation_fv_rs_ctx = "${aci_vrf.cisco_it_vrf.name}"
+	name               = "terraform-jaheo-bd"
+}
+
+resource "aci_subnet" "cisco_it_subnet" {
+	bridge_domain_dn   = "${aci_bridge_domain.cisco_it_bd.name}"
+	name               = "terraform-jaheo-subnet"
+	ip                 = "10.23.239.1/22"
+}
+
 module "aci_l3out" {
   source  = "qzx/l3out/aci"
   version = "1.0.0"
